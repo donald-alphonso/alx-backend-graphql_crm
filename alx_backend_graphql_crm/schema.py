965005e2ddc60b4a3cf9c709f823930b9eeec872
donald.ahossi <donaldalphonso11@gmail.com>
2025-06-29 19:07:35 +0200

    tache 0

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -1,21 +1,9 @@
 import graphene
-from crm.schema import Query as CrmQuery, Mutation as CrmMutation
 
 class Query(graphene.ObjectType):
-    # Define your query fields here
-    hello = graphene.String()
+    hello = graphene.String(description="A simple greeting")
 
-    def resolve_hello(self, info):
-        return "Hello, GraphQL!" 
-# class Query(CrmQuery, graphene.ObjectType):
-#     """
-#     The root Query class for the GraphQL API, combining CRM queries.
-#     """
-#     pass
-class Mutation(CrmMutation, graphene.ObjectType):
-    """
-    The root Mutation class for the GraphQL API, combining CRM mutations.
-    """
-    pass
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
+        
+schema = graphene.Schema(query=Query)
